d1907c53ea8da37c433005484de806810f476538
TEST <test@testing.local>
2023-03-13 02:05:03 +0500

    initial commit

---
+++ src/products/products.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Post, Body, Param, Patch, Delete } from '@nestjs/common';
+import { ProductsService } from './products.service';
+
+@Controller("products")
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Post()
+  // addProduct(@Body() body: { title: string, description: string, price: number }) -- this is the same as the next line
+  addProduct(@Body('title') title: string, @Body('description') description: string, @Body('price') price: number ): any {
+    const generatedId =  this.productsService.insertProduct(title, description, price);
+    return { id: generatedId };
+  }
+
+  @Get()
+  getProducts() {
+    return this.productsService.getProducts();
+  }
+
+  @Get(':id')
+  getProduct(@Param('id') id: string) {
+    return this.productsService.getProduct(id);
+  }
+
+  @Patch(':id')
+  updateProduct(@Param('id') id: string, @Body('title') title: string, @Body('description') description: string, @Body('price') price: number) {
+    return this.productsService.updateProduct(id, title, description, price);
+  }
+
+  @Delete(':id')
+  deleteProduct(@Param('id') id: string) {
+    return this.productsService.deleteproduct(id);
+  }
+}
